5c24be921cbc462850818f8645a2fbbd1b005a55
Katamar <marina.katava@gmail.com>
2014-04-12 16:10:28 +0200

    code that calculates msf in a timewindow, split in functions

--- analysis/MSF/msfFunction.py
+++ analysis/MSF/msfFunction.py
@@ -3,8 +3,29 @@ import MDAnalysis.KDTree.NeighborSearch as NS
 import MDAnalysis.analysis.align as AL
 import numpy as np
 
+def accumulate_positions(lenc, coord, x_avg, y_avg, z_avg, x_a, y_a, z_a):
+    for j in range(0,lenc):
+        x_avg[j]+=coord[j][0]
+        y_avg[j]+=coord[j][1]
+        z_avg[j]+=coord[j][2]
+        x_a[j]+=coord[j][0]*coord[j][0]
+        y_a[j]+=coord[j][1]*coord[j][1]
+        z_a[j]+=coord[j][2]*coord[j][2]
+    return x_avg, y_avg, z_avg, x_a, y_a, z_a
 
-def msf(begin, end, ref, trj, fixedWindowWidth, lenH, H_Catoms):
+def average_positions(lenc, fixedWindowWidth, x_avg, y_avg, z_avg, x_a, y_a, z_a):
+    finalval=0
+    for j in range(0,lenc):
+        x_avg[j]=(x_avg[j]/fixedWindowWidth)*(x_avg[j]/fixedWindowWidth)
+        y_avg[j]=(y_avg[j]/fixedWindowWidth)*(y_avg[j]/fixedWindowWidth)
+        z_avg[j]=(z_avg[j]/fixedWindowWidth)*(z_avg[j]/fixedWindowWidth)
+        x_a[j]=x_a[j]/fixedWindowWidth
+        y_a[j]=y_a[j]/fixedWindowWidth
+        z_a[j]=z_a[j]/fixedWindowWidth
+        finalval+=1.0/3.0*(x_a[j]-x_avg[j]+y_a[j]-y_avg[j]+z_a[j]-z_avg[j])
+    return finalval
+
+def msf_win(begin, end, ref, trj, fixedWindowWidth, lenH, H_Catoms, u1, u2):
    x_avg=[0]*lenH
    y_avg=[0]*lenH
    z_avg=[0]*lenH
@@ -15,25 +36,10 @@ def msf(begin, end, ref, trj, fixedWindowWidth, lenH, H_Catoms):
    for ts in u2.trajectory[begin:end]:
       result=AL.alignto(trj, ref, select="name CA")
       coord=H_Catoms.atoms.coordinates()
-      for j in range(0,len(coord)):
-         x_avg[j]+=coord[j][0]
-         y_avg[j]+=coord[j][1]
-         z_avg[j]+=coord[j][2]
-         x_a[j]+=coord[j][0]*coord[j][0]
-         y_a[j]+=coord[j][1]*coord[j][1]
-         z_a[j]+=coord[j][2]*coord[j][2]
-
-   for j in range(0,len(coord)):
-      x_avg[j]=(x_avg[j]/fixedWindowWidth)*(x_avg[j]/fixedWindowWidth)
-      y_avg[j]=(y_avg[j]/fixedWindowWidth)*(y_avg[j]/fixedWindowWidth)
-      z_avg[j]=(z_avg[j]/fixedWindowWidth)*(z_avg[j]/fixedWindowWidth)
-      x_a[j]=x_a[j]/fixedWindowWidth
-      y_a[j]=y_a[j]/fixedWindowWidth
-      z_a[j]=z_a[j]/fixedWindowWidth
-      finalval+=1.0/3.0*(x_a[j]-x_avg[j]+y_a[j]-y_avg[j]+z_a[j]-z_avg[j])
-      #print finalval
-   finalval=finalval/len(coord)  
-   return finalval 
+      accumulate_positions(len(coord), coord, x_avg, y_avg, z_avg, x_a, y_a, z_a)
+   win_avg=average_positions(len(coord), fixedWindowWidth, x_avg, y_avg, z_avg, x_a, y_a, z_a)
+   win_avg/=float(len(coord))
+   return win_avg 
 
 
 def main():
@@ -62,7 +68,7 @@ def main():
         ref = u1.selectAtoms("segid %s" %(i))
         trj = u2.selectAtoms("segid %s" %(i))
         
-        print msf(begin, end, ref, trj, fixedWindowWidth, lenH, H_Catoms)
+        print msf_win(begin, end, ref, trj, fixedWindowWidth, lenH, H_Catoms, u1, u2)
 
 
 if __name__ == "__main__":
