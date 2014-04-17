0d2fe4856411d32f9fd69cd62939699b262868cd
Katamar <marina.katava@gmail.com>
2014-04-17 14:26:18 +0200

    code that should work for both monomer and powder, no print statements

--- analysis/MSD/msdFunction.py
+++ analysis/MSD/msdFunction.py
@@ -24,53 +24,33 @@ def msd_block(selection, u1, u2, begin, timeframe, blocklen):
         ns_hy=NS.AtomNeighborSearch(hy)
         H_atoms=ns_hy.search_list(car, 1.5)
         lenH=len(H_atoms)
-        ctr=0  #delete later
-        #print begin, begin+blocklen-2
         for ts in u2.trajectory[begin:begin+blocklen-timeframe+1]:         #u2.trajectory[0,3]: means 0, 1, 2 --> last one doesn't count!
-            ctr+=1
             u2.trajectory[begin]
             AL.alignto(segment, ref, select="name CA")
             startframe=H_atoms.atoms.coordinates()
             u2.trajectory[begin+timeframe-1]
             AL.alignto(segment, ref, select="name CA")
             stopframe=H_atoms.atoms.coordinates()
-            #if begin>35000:
-            print 'begin, end', begin, begin+timeframe-1
             msd_bl+=msd_win(lenH, startframe, stopframe)
-            #print msd_bl
             begin+=1
-        #print 'i went through this many timewindows, theoretical value', ctr, blocklen-timeframe+1 
         msd_bl/=float(blocklen-timeframe+1)
     msd_bl/=len(selection)
     return msd_bl 
 
 
 def main():
-    #pdb="2lym_wbi.pdb"
-    #psf="2lym_wbi.psf"
-    #dcd="traj.dcd"
-    #pdb="2lym_dis_run.pdb"
-    #psf="2lym_dis_run.psf"
-    #dcd1="250.production.4.dcd"
-    #dcd2="250.production.5.dcd"
-    #dcd="250.production.5.dcd"
     pdb="unwrapped_disulfide_run.pdb"
     psf="unwrapped_disulfide_run.psf"
     dcd="powtraj.dcd"
     f=open('powder_250.dat', 'w')
     u1 = MDAnalysis.Universe(pdb)
     u2 = MDAnalysis.Universe(psf, dcd)
-    #u2 = MDAnalysis.Universe(psf, dcd)
-    print len(u2.trajectory)
-    timeframe=3
+    timeframe=300
     begin=0
-    blocklen=7
+    blocklen=20000
     selection='ABCDEFGH'
 
-    #print msd_block(selection, u1, u2, begin, timeframe, blocklen)
     for i in range(0, len(u2.trajectory)/blocklen*blocklen, blocklen):
-        print 'new block starts here', i
-        print 'ovo je zadnja petlja ikada'
         begin=i
         print >> f, msd_block(selection, u1, u2, begin, timeframe, blocklen)
 
